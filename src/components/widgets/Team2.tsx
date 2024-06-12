8955b08421c263853c19c5bfd006c2dc98987ee8
Susnfor <97429786+Susnfor@users.noreply.github.com>
2024-06-13 00:51:19 +0100

    type error fix

--- src/components/widgets/Team2.tsx
+++ src/components/widgets/Team2.tsx
@@ -8,7 +8,7 @@ const Team = ({ header, teams, id, hasBackground = false }: TeamProps) => (
     {header && <Headline header={header} titleClass="text-2xl sm:text-3xl" />}
     <div className="flex items-stretch justify-center">
       <div className="grid grid-cols-1 gap-4 dark:text-white sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4">
-        {teams.map(({ name, occupation, image, items }, index) => (
+        {teams && teams.map(({ name, occupation, image, items }, index) => (
           <div key={`item-team-two-${index}`} className="p-2">
             <ItemTeam
               name={name}

ce642624e08d2931fec51c1ca46dffc1f35abd4e
Susnfor <97429786+Susnfor@users.noreply.github.com>
2024-06-13 00:38:26 +0100

    fix: branding

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -6,8 +6,8 @@ addons.setConfig({
     base: 'light',
 
     // Logo
-    brandTitle: 'TailNext',
-    brandUrl: 'https://github.com/onwidget/tailnext',
+    brandTitle: 'JnB',
+    brandUrl: '#',
     brandTarget: '_blank',
   }),
 });

37ad89d4433e6d3a9ee597dae33b551c251d51c5
Susnfor <97429786+Susnfor@users.noreply.github.com>
2024-06-12 20:45:42 +0100

    init

---
+++ src/shared/data/global.data.tsx
@@ -0,0 +1,273 @@
+import {
+  IconBrandFacebook,
+  IconBrandGithub,
+  IconBrandInstagram,
+  IconBrandLinkedin,
+  IconBrandTwitter,
+  IconChevronDown,
+  IconRss,
+} from '@tabler/icons-react';
+import { AnnouncementProps, FooterProps, HeaderProps } from '../types';
+
+// Announcement data
+export const announcementData: AnnouncementProps = {
+  title: 'NEW',
+  callToAction: {
+    text: 'This template is made with Next.js 14 using the new App Router »',
+    href: 'https://nextjs.org/blog/next-14',
+  },
+  callToAction2: {
+    text: 'Follow @onWidget on Twitter',
+    href: 'https://twitter.com/intent/user?screen_name=onwidget',
+  },
+};
+
+// Header data
+export const headerData: HeaderProps = {
+  links: [
+    // {
+    //   label: 'Services',
+    //   icon: IconChevronDown,
+    //   links: [
+    //     {
+    //       label: 'Social Media',
+    //       href: '/services',
+    //     },
+    //     {
+    //       label: 'Podcasts',
+    //       href: '/pricing',
+    //     },
+    //     // {
+    //     //   label: 'About us',
+    //     //   href: '/about',
+    //     // },
+    //     // {
+    //     //   label: 'Contact us',
+    //     //   href: '/contact',
+    //     // },
+    //     // {
+    //     //   label: 'FAQs',
+    //     //   href: '/faqs',
+    //     // },
+    //     // {
+    //     //   label: 'Terms & Conditions',
+    //     //   href: '/terms',
+    //     // },
+    //     // {
+    //     //   label: 'Privacy Policy',
+    //     //   href: '/privacy',
+    //     // },
+    //   ],
+    // },
+    {
+      label: 'Services',
+      href: '/services',
+    },
+    {
+      label: 'About Us',
+      href: '/about',
+    },
+    {
+      label: 'Pricing',
+      href: '/pricing',
+    },
+  ],
+  actions: [
+    {
+      text: 'Contact Us',
+      href: '/contact',
+      targetBlank: true,
+    },
+  ],
+  isSticky: true,
+  showToggleTheme: true,
+  showRssFeed: false,
+  position: 'right',
+};
+
+// Footer data
+export const footerData: FooterProps = {
+  title: 'TailNext',
+  links: [
+    {
+      label: 'Terms & Conditions',
+      href: '/terms',
+    },
+    {
+      label: 'Privacy Policy',
+      href: '/privacy',
+    },
+  ],
+  columns: [
+    {
+      title: 'Product',
+      links: [
+        {
+          label: 'Features',
+          href: '/',
+        },
+        {
+          label: 'Security',
+          href: '/',
+        },
+        {
+          label: 'Team',
+          href: '/',
+        },
+        {
+          label: 'Enterprise',
+          href: '/',
+        },
+        {
+          label: 'Customer stories',
+          href: '/',
+        },
+        {
+          label: 'Pricing',
+          href: '/pricing',
+        },
+        {
+          label: 'Resources',
+          href: '/',
+        },
+      ],
+    },
+    {
+      title: 'Platform',
+      links: [
+        {
+          label: 'Developer API',
+          href: '/',
+        },
+        {
+          label: 'Partners',
+          href: '/',
+        },
+      ],
+    },
+    {
+      title: 'Support',
+      links: [
+        {
+          label: 'Docs',
+          href: '/',
+        },
+        {
+          label: 'Community Forum',
+          href: '/',
+        },
+        {
+          label: 'Professional Services',
+          href: '/',
+        },
+        {
+          label: 'Skills',
+          href: '/',
+        },
+        {
+          label: 'Status',
+          href: '/',
+        },
+      ],
+    },
+    {
+      title: 'Company',
+      links: [
+        {
+          label: 'About',
+          href: '/',
+        },
+        {
+          label: 'Blog',
+          href: '/blog',
+        },
+        {
+          label: 'Careers',
+          href: '/',
+        },
+        {
+          label: 'Press',
+          href: '/',
+        },
+        {
+          label: 'Inclusion',
+          href: '/',
+        },
+        {
+          label: 'Social Impact',
+          href: '/',
+        },
+        {
+          label: 'Shop',
+          href: '/',
+        },
+      ],
+    },
+  ],
+  socials: [
+    { label: 'Twitter', icon: IconBrandTwitter, href: '#' },
+    { label: 'Instagram', icon: IconBrandInstagram, href: '#' },
+    { label: 'Facebook', icon: IconBrandFacebook, href: '#' },
+    { label: 'RSS', icon: IconRss, href: '#' },
+    { label: 'Github', icon: IconBrandGithub, href: 'https://github.com/onwidget/tailnext' },
+  ],
+  footNote: (
+    <div className="mr-4 text-sm">
+      <a
+        className="font-semibold text-slate-900 dark:text-gray-200 hover:text-blue-600 hover:underline dark:hover:text-blue-600"
+        href="https://onwidget.com/"
+      >
+        {' '}
+        JnB
+      </a>{' '}
+      · All rights reserved.
+    </div>
+  ),
+};
+
+// Footer2 data
+export const footerData2: FooterProps = {
+  links: [
+    {
+      label: 'Terms & Conditions',
+      href: '/terms',
+    },
+    {
+      label: 'Privacy Policy',
+      href: '/privacy',
+    },
+  ],
+  columns: [
+    {
+      title: 'Address',
+      texts: ['51 Phasellus Avenue Maecenas', 'Aliquam, AQ 52098'],
+    },
+    {
+      title: 'Phone',
+      texts: ['Reception: +105 123 4567', 'Office: +107 235 7890'],
+    },
+    {
+      title: 'Email',
+      texts: ['Office: info@example.com', 'Site: https://example.com'],
+    },
+  ],
+  socials: [
+    { label: 'Twitter', icon: IconBrandLinkedin, href: 'https://www.linkedin.com/company/jnb-production-studios/' },
+    { label: 'Instagram', icon: IconBrandInstagram, href: 'https://www.instagram.com/jnb_productionstudios/' },
+    // { label: 'Facebook', icon: IconBrandFacebook, href: '#' },
+    // { label: 'RSS', icon: IconRss, href: '#' },
+    // { label: 'Github', icon: IconBrandGithub, href: 'https://github.com/onwidget/tailnext' },
+  ],
+  footNote: (
+    <div className="mr-4 text-sm">
+      <a
+        className="font-semibold text-slate-900 dark:text-gray-200 hover:text-blue-600 hover:underline dark:hover:text-blue-600"
+        href="#"
+      >
+        {' '}
+        JnB Production Studios LTD
+      </a>{' '}
+      · All rights reserved.
+    </div>
+  ),
+};
